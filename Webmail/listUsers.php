ed576a83168aaa58860c6583259e05bfab926008
Dennis de Best <Dennis@debest.fr>
2016-04-13 14:07:55 +0200

    set quota added to addmailuser

--- Webmail/listUsers.php
+++ Webmail/listUsers.php
@@ -1,8 +1,8 @@
 <?php
 
-$sysDirs = array(".", "..", "bash", "Webmail", "index.php", "plugins", ".gitignore");
+$sysDirs = array(".", "..", "bash", "Webmail", "index.php", "plugins", ".gitignore", "aquota.user");
 
-$dirs = scandir('/var/www/html');
+$dirs = scandir('/mnt/disk/');
 
 
 foreach ($dirs as $dir){
@@ -12,7 +12,7 @@ foreach ($dirs as $dir){
 echo "<table>";
 foreach ($dirsArray as $user){
 
-    echo "<form class='usersform' method='post' action='delmailuser.php'>";
+    echo "<form class='usersform' method='post' action='delUser.php'>";
     echo "<tr>";
     echo "<th><label>".$user."</label></th>";
     echo "<input type='hidden' name='username' value='".$user."'/>";
