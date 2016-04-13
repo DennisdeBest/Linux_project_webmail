ccfc68733c318eefc76d3fa62c60205aa1dc398c
Dennis de Best <Dennis@debest.fr>
2016-04-13 20:34:21 +0200

    bug fixes on delvHost

--- Webmail/delvHost.php
+++ Webmail/delvHost.php
@@ -3,9 +3,9 @@ session_start();
 $_SESSION['output'] = "";
 $_SESSION['error'] = "";
 
-if(!empty($_POST['username'])){
-    $username = $_POST['username'];
-    $script = "/var/www/html/Webmail/bash/delmailuser ".$username;
+if(!empty($_POST['vhost'])){
+    $vhost = $_POST['vhost'];
+    $script = "/var/www/html/Webmail/bash/delvHost ".$vhost;
     shell_exec($script);
 
     header("Location:http://".$_SERVER['HTTP_HOST'].$_SESSION['referrer']);
